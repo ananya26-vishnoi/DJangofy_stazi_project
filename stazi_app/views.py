a116047a3be1ae746aea9e0f77daec7f22952bbe
ananya26-vishnoi <ananyavishnoi26@gmail.com>
2023-07-13 22:09:41 +0530

    auction

--- stazi_app/views.py
+++ stazi_app/views.py
@@ -1,18 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import *
-from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse
-import os
-from dotenv import load_dotenv
-from django.utils import timezone
-from .serializers import *
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.decorators import api_view
-from django.db.models import Max
 
-    
 
 import random
 import string
