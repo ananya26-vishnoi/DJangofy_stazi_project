602a5207e8bfcd1b0b5638546624ab061ca3e3da
ananya26-vishnoi <ananyavishnoi26@gmail.com>
2023-08-15 21:37:16 +0530

    auction

--- stazi_app/views.py
+++ stazi_app/views.py
@@ -32,3 +32,43 @@ def create_user(request):
     user_serializer=UserSerializer(user)
     return Response(user_serializer.data,status=status.HTTP_201_CREATED)
 
+@api_view(['PUT'])
+def update_user(request):
+    if "email" not in request.data:
+        return Response({"error":"email is required"},status=status.HTTP_400_BAD_REQUEST)
+    if "password" not in request.data:
+        return Response({"error":"password is required"},status=status.HTTP_400_BAD_REQUEST)
+    
+    email=request.data["email"]
+    password=request.data["password"]
+
+    if not User.objects.filter(email=email, password=password).exists():
+        return Response({"error":"email or password id wrong"},status=status.HTTP_400_BAD_REQUEST)
+
+    if "new_password" in request.data:
+        new_password=request.data["new_password"]
+        User.objects.filter(email=email).update(password=new_password)
+
+    if "username" in request.data:
+        username=request.data["username"]
+        User.objects.filter(email=email).update(username=username)
+    user=User.objects.get(email=email)
+    user_serializer=UserSerializer(user)
+    return Response(user_serializer.data,status=status.HTTP_200_OK)
+
+@api_view(['DELETE'])
+def delete_user(request):
+    if "email" not in request.data:
+        return Response({"error":"email is required"},status=status.HTTP_400_BAD_REQUEST)
+    if "password" not in request.data:
+        return Response({"error":"password is required"},status=status.HTTP_400_BAD_REQUEST)
+    
+    email=request.data["email"]
+    password=request.data["password"]
+
+    if not User.objects.filter(email=email, password=password).exists():
+        return Response({"error":"profile does not exist"},status=status.HTTP_400_BAD_REQUEST)
+    user=User.objects.get(email=email)
+    user.delete()
+    return Response({"success":"user deleted"},status=status.HTTP_200_OK)
+
