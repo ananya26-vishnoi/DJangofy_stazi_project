9f7f15fe2a1f7119e9dfe29c39ec4bdc233c1649
ananya26-vishnoi <ananyavishnoi26@gmail.com>
2023-08-17 21:36:53 +0530

    auction

--- stazi_app/views.py
+++ stazi_app/views.py
@@ -15,6 +15,20 @@ from django.db.models import Max
 import random
 import string
 
+@api_view(['POST'])
+def create_user(request):
+    if "username" not in request.data or "email" not in request.data or "password" not in request.data:
+        return Response({"error":"username, email, password, and role are required"},status=status.HTTP_400_BAD_REQUEST)
     
+    username=request.data["username"]
+    email=request.data["email"]
+    password=request.data["password"]
     
-    
+    if User.objects.filter(email=email).exists():
+        return Response({"error":"email already exists"},status=status.HTTP_400_BAD_REQUEST)
+        
+    user=User.objects.create(username=username,email=email,password=password,role="user")
+    user.save()
+    user_serializer=UserSerializer(user)
+    return Response(user_serializer.data,status=status.HTTP_201_CREATED)
+
