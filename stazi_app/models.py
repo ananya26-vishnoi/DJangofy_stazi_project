4187f783c33c7e0cc7d1157711c25034c23e742d
Ananya Vishnoi <71982267+ananya26-vishnoi@users.noreply.github.com>
2023-10-03 23:02:39 +0530

    commit message

---
+++ stazi_app/models.py
@@ -0,0 +1,20 @@
+from django.db import models 
+class User(models.Model):
+    username = models.CharField(max_length=1000)
+    email = models.EmailField()
+    password = models.CharField(max_length=1000)
+    role = models.CharField(max_length=1000)
+class Token(models.Model):
+    user_id = models.ForeignKey(on_delete=models.CASCADE)
+    token_value = models.CharField(max_length=1000)
+class Auction(models.Model):
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    start_price = models.IntegerField(default=0)
+    item_name = models.CharField(max_length=1000)
+    winner_user_id = models.ForeignKey(on_delete=models.CASCADE)
+class Bid(models.Model):
+    auction_id = models.ForeignKey(on_delete=models.CASCADE)
+    user_id = models.ForeignKey(on_delete=models.CASCADE)
+    bid_amount = models.IntegerField(default=0)
+    timestamp = models.TimeField()
